f0a86bac8932db2d10cbbe8ec3dac00ad5627cf3
Nick Wogan <nicholaswogan@gmail.com>
2024-02-12 08:30:39 -0800

    test 3c and 3d

--- archean.py
+++ archean.py
@@ -114,7 +114,53 @@ def test3b(savefile=False):
 
     if savefile:
         utils.pie_output_file(pc,'slices/ArcheanEarth/test3/test3b/ArcheanEarth_test3b.txt')
-        
+
+def test3c(savefile=False):
+    print('\nRunning test3c')
+    pc = Atmosphere("reactions/zahnle_earth.yaml",\
+                "./slices/ArcheanEarth/test3/test3c/settings_ArcheanEarth_test3c.yaml",\
+                "./slices/ArcheanEarth/test2/Sun_3.8Ga_energyunits_edited.txt",\
+                "./slices/ArcheanEarth/test3/test3c/atmosphere_ArcheanEarth_test3c.txt")
+    
+    # Turn off molecular diffusion for all species but H2 and H
+    @nb.cfunc(nb.double(nb.double, nb.double, nb.double))
+    def custom_binary_diffusion_fcn(mu_i, mubar, T):
+        if mu_i < 2.01595:
+            b = 1.52e18*((1.0/mu_i+1.0/mubar)**0.5)*(T**0.5)
+        else:
+            b = 0.0    
+        return b
+    pc.var.custom_binary_diffusion_fcn = custom_binary_diffusion_fcn
+    
+    pc.initialize_stepper(pc.wrk.usol)
+    tn = 0.0
+    while tn < pc.var.equilibrium_time:
+        tn = pc.step()
+
+    if savefile:
+        utils.pie_output_file(pc,'slices/ArcheanEarth/test3/test3c/ArcheanEarth_test3c.txt')
+
+def test3d(savefile=False):
+    print('\nRunning test3d')
+    pc = Atmosphere("reactions/zahnle_earth.yaml",\
+                "./slices/ArcheanEarth/test3/test3d/settings_ArcheanEarth_test3d.yaml",\
+                "./slices/ArcheanEarth/test2/Sun_3.8Ga_energyunits_edited.txt",\
+                "./slices/ArcheanEarth/test3/test3d/atmosphere_ArcheanEarth_test3d.txt")
+    
+    @nb.cfunc(nb.double(nb.double, nb.double, nb.double))
+    def custom_binary_diffusion_fcn(mu_i, mubar, T):
+        b = 1.52e18*((1.0/mu_i+1.0/mubar)**0.5)*(T**0.5)
+        return b
+    pc.var.custom_binary_diffusion_fcn = custom_binary_diffusion_fcn
+    
+    pc.initialize_stepper(pc.wrk.usol)
+    tn = 0.0
+    while tn < pc.var.equilibrium_time:
+        tn = pc.step()
+
+    if savefile:
+        utils.pie_output_file(pc,'slices/ArcheanEarth/test3/test3d/ArcheanEarth_test3d.txt')
+
 if __name__ == "__main__":
     savefile = False
     test0(savefile=savefile)
@@ -124,3 +170,5 @@ if __name__ == "__main__":
     test2(savefile=savefile)
     test3a(savefile=savefile)
     test3b(savefile=savefile)
+    test3c(savefile=savefile)
+    test3d(savefile=savefile)
